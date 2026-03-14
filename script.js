0abf105a2e4050a457613ed10d99eb56d8b65ed2
bhumikkohar <114809176+bhumikkohar@users.noreply.github.com>
2026-03-15 04:07:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function calculate(operator) {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    let result;
+
+    if (operator === "+") result = num1 + num2;
+    if (operator === "-") result = num1 - num2;
+    if (operator === "*") result = num1 * num2;
+    if (operator === "/") result = num1 / num2;
+
+    document.getElementById("result").innerText = "Result: " + result;
+
+}
